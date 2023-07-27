c5112ab46837983ca68f2659f4836dc180faba24
shinhyehwa <nguyenhongxd2002@gmail.com>
2023-07-27 10:23:45 +0700

    create view of fragment stop watch

--- app/src/main/java/com/example/alarmapp/Fragment/FragmentStopWatch.kt
+++ app/src/main/java/com/example/alarmapp/Fragment/FragmentStopWatch.kt
@@ -1,15 +1,24 @@
 package com.example.alarmapp.Fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.alarmapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class FragmentStopWatch : Fragment() {
 
-
+    private lateinit var textStopWatch: TextView
+    private lateinit var btnStop: FloatingActionButton
+    private lateinit var btnStopTime: FloatingActionButton
+    private lateinit var btnStart: FloatingActionButton
+    private lateinit var listTimeStop: RecyclerView
+    private var isPlaying: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,8 +29,40 @@ class FragmentStopWatch : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stop_watch, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_stop_watch, container, false)
+        init(view)
+        return view
     }
 
+    private fun init(view: View){
+        textStopWatch = view.findViewById(R.id.text_stop_watch)
+        btnStart = view.findViewById(R.id.btnStart)
+        btnStopTime = view.findViewById(R.id.btnStopTime)
+        btnStop = view.findViewById(R.id.btnStop)
+
+        btnStart.setOnClickListener {
+            showFloatingActionButton()
+            isPlaying = true
+            Log.e("khongbiet", "khong chay duoc")
+        }
+        btnStop.setOnClickListener {
+            showFloatingActionButton()
+        }
+        btnStopTime.setOnClickListener {
+            textStopWatch.text = "00:00.16"
+        }
+    }
+
+    private fun showFloatingActionButton(){
+        if(!isPlaying){
+            btnStart.visibility = View.GONE
+            btnStopTime.visibility = View.VISIBLE
+            btnStop.visibility = View.VISIBLE
+        }else{
+            btnStart.visibility = View.VISIBLE
+            btnStopTime.visibility = View.GONE
+            btnStop.visibility = View.GONE
+        }
+    }
 
 }
